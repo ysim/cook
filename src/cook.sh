d35be5e6c4be4689db38f504a811e866430e674c
Yi Qing Sim <devnull@yiqingsim.net>
2017-04-10 22:36:59 +0200

    src/cook.sh: add view_markdown() using pandoc and lynx to view markdown

--- src/cook.sh
+++ src/cook.sh
@@ -10,12 +10,17 @@ die() {
     exit 1
 }
 
+view_markdown() {
+    markdown_file=$1
+    "$(which pandoc)" "${markdown_file}" | "$(which lynx)" -stdin
+}
+
 cmd_show() {
     local name="$1"
     local recipe_path="${RECIPES_DIR}/${name}${RECIPES_EXTENSION}"
 
     if [[ -f $recipe_path ]]; then
-        cat "$recipe_path"
+        view_markdown "${recipe_path}"
     else
         die "No recipe exists at the path: ${recipe_path}"
     fi
