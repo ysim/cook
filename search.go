d6f139332b839b787604c72ebe39f836e00873a2
Yi Qing Sim <devnull@yiqingsim.net>
2017-05-02 22:41:18 +0200

    search.go: change key/value symbol from = to :

--- search.go
+++ search.go
@@ -49,7 +49,7 @@ func Search(args []string) {
 	// For now, just allow simple searching, for one value on one field, e.g.
 	// "ingredients=chicken"
 	argString := strings.Join(args[:], " ")
-	keyValue := strings.Split(argString, "=")
+	keyValue := strings.Split(argString, ":")
 
 	// strings.Split will always return an array of at least one item
 	// (if there are no matches, that item will be an empty string)
