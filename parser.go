13911fc8c94780a03a4eb64b75cf54d7319a2a23
Yi Qing Sim <devnull@yiqingsim.net>
2017-06-06 11:34:21 +0200

    parser.go: output plain text with no prettifying by default

--- parser.go
+++ parser.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/russross/blackfriday"
+	"golang.org/x/net/html"
+	"strings"
 )
 
+func ParseHTML(htmlBytes []byte) ([]string, error) {
+	var output []string
+	z := html.NewTokenizer(bytes.NewReader(htmlBytes))
+	for {
+		token := z.Next()
+		switch token {
+		case html.ErrorToken:
+			return output, z.Err()
+		case html.TextToken:
+			s := z.Text()
+			output = append(output, string(s))
+		}
+	}
+}
+
 func RenderMarkdown(mdBytes []byte) error {
-	// TODO: decide how to render the markdown
-	output := blackfriday.MarkdownBasic(mdBytes)
-	fmt.Println(output)
+	htmlBytes := blackfriday.MarkdownBasic(mdBytes)
+	outputSlice, err := ParseHTML(htmlBytes)
+	if err.Error() != "EOF" {
+		return err
+	}
+	fmt.Println(strings.Join(outputSlice, ""))
 	return nil
 }
