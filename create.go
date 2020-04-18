15be2d9d729af0f6fb9d05b6ce518f8d1de89408
ysim <devnull@yiqingsim.net>
2020-04-17 20:59:09 -0400

    Slight correction to the comment about os.IsExist blocks. (#12)

--- create.go
+++ create.go
@@ -60,7 +60,7 @@ func validateNewRecipe(filename string) string {
 
 	// Not the same as os.IsExist! This is because os.Stat doesn't throw an
 	// error if the file exists, so os.IsExist would receive a nil value for err.
-	// So, a os.IsExist(err) block would never execute.
+	// So, an os.IsExist(err) block would never execute for a file that exists.
 	if !os.IsNotExist(err) {
 		fmt.Printf("There already exists a file at the path: %s\n", filepath)
 		os.Exit(1)
