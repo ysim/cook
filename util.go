a870951546f36a3e91a83f7e1e6b129da0e3900f
Yi Qing Sim <yiqing.sim@shopify.com>
2020-07-24 15:42:09 -0400

    Move StringInSlice function to util.go. (#23)

--- util.go
+++ util.go
@@ -4,6 +4,15 @@ import (
 	"fmt"
 )
 
+func StringInSlice(s string, slice []string) bool {
+	for _, i := range slice {
+		if s == i {
+			return true
+		}
+	}
+	return false
+}
+
 func PrintArrayOnNewlines(a []string) {
 	for _, v := range a {
 		fmt.Println(v)
